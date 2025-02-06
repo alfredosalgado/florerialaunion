6b75aa0fd135cb25eea3cfc04f26f628704e0fad
alfredosalgado <alfresal88@gmail.com>
2025-02-06 01:52:53 -0300

    all2

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+// main.js
+document.querySelector('.whatsapp-btn').addEventListener('click', function() {
+  const phone = '1234567890'; // Número de teléfono
+  const message = 'Hola, estoy interesado en más información.'; // Mensaje predefinido
+  window.open(`https://wa.me/${phone}?text=${encodeURIComponent(message)}`, '_blank');
+});
+
+
+// main.js o directamente en el archivo <script> de tu HTML
+
+// Obtén el botón de "Subir"
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Cuando el usuario haga scroll hacia abajo, muestra el botón
+window.onscroll = function () {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    scrollToTopBtn.style.display = "flex"; // Muestra el botón
+  } else {
+    scrollToTopBtn.style.display = "none"; // Oculta el botón
+  }
+};
+
+// Cuando el usuario haga clic en el botón, realiza el desplazamiento suave
+scrollToTopBtn.addEventListener("click", function (event) {
+  event.preventDefault(); // Evita la acción predeterminada de ancla
+
+  // Desplazamiento suave hasta la parte superior
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Añade la transición suave
+  });
+});
